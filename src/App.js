1b19774a2581543befce44399c59326d20e48146
JustinRoderick <justinroderick258@gmail.com>
2024-02-22 13:20:54 -0500

    Update js files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import './App.css';
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from './pages/LoginPage';
 import CardPage from './pages/CardPage';
 function App()
 {
-return (
-<CardPage />
-);
+    return (
+        <BrowserRouter>
+        <Routes>
+        <Route path="/" index element={<LoginPage />} />
+        <Route path="/cards" index element={<CardPage />} />
+        </Routes>
+        </BrowserRouter>
+        );
 }
 
 export default App;
