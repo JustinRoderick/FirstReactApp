f3d50c143ee19d43efcd8f919e8fd46634d54a4e
JustinRoderick <justinroderick258@gmail.com>
2024-02-22 17:52:55 -0500

    Update js files from Doc

--- src/components/LoggedInName.js
+++ src/components/LoggedInName.js
@@ -1,18 +1,22 @@
 import React from 'react';
 function LoggedInName()
 {
-var user={}
-const doLogout = event =>
-{
-event.preventDefault();
-alert('doLogout');
-};
-return(
-<div id="loggedInDiv">
-<span id="userName">Logged In As John Doe </span><br />
-<button type="button" id="logoutButton" class="buttons"
-onClick={doLogout}> Log Out </button>
-</div>
-);
+    var _ud = localStorage.getItem('user_data');
+    var ud = JSON.parse(_ud);
+    var userId = ud.id;
+    var firstName = ud.firstName;
+    var lastName = ud.lastName;
+
+    const doLogout = event =>{
+        event.preventDefault();
+        localStorage.romoveItem("user_datadata")
+        window.location.href = '/';
+    };
+    return(
+        <div id="loggedInDiv">
+            <span id="userName">Logged In As John Doe </span><br />
+            <button type="button" id="logoutButton" class="buttons" onClick={doLogout}> Log Out </button>
+        </div>
+    );
 };
 export default LoggedInName;
